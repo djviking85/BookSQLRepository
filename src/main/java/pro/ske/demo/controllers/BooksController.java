0548e92626bf1bf98cef9e754772dd251b707f8c
Viking <djviking@yandex.ru>
2023-04-28 17:50:21 +0300

    делаем практику SQL по BOOK

--- src/main/java/pro/ske/demo/controllers/BooksController.java
+++ src/main/java/pro/ske/demo/controllers/BooksController.java
@@ -2,10 +2,16 @@ package pro.ske.demo.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import pro.ske.demo.model.Book;
+import pro.ske.demo.services.BookCoverService;
 import pro.ske.demo.services.BookService;
+
+import javax.persistence.Entity;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/books")
@@ -26,23 +32,27 @@ public class BooksController {
 
     @GetMapping
 //    (required = false) параметр или может ыбть указан или нет
-    public ResponseEntity findBooks(@RequestParam (required = false) String name,
-                                    @RequestParam (required = false) String autor,
-                                    @RequestParam (required = false) String part,
-                                    @RequestParam (required = false) Long id) {
+    public ResponseEntity findBooks(@RequestParam(required = false) String name,
+                                    @RequestParam(required = false) String autor,
+                                    @RequestParam(required = false) String part,
+                                    @RequestParam(required = false) Long id) {
 
         // если имя, автор, частьслова не налл и имя, автьор или часть слова не состоящаая из пробелов и пустоты
-        if (name != null && !name.isBlank()){
+        if (name != null && !name.isBlank()) {
             return ResponseEntity.ok(bookService.findByNameContainingIgnoreCase(name));
-        } if (autor != null && !autor.isBlank()){
+        }
+        if (autor != null && !autor.isBlank()) {
             return ResponseEntity.ok(bookService.findBookByAutorContainsIgnoreCase(autor));
-        } if (part != null && !part.isBlank()) {
+        }
+        if (part != null && !part.isBlank()) {
             return ResponseEntity.ok(bookService.findAllByNameContains(part));
 
-        } if (name != null && !name.isBlank()) {
+        }
+        if (name != null && !name.isBlank()) {
             return ResponseEntity.ok(bookService.findBooksByNameAndAutorIgnoreCase(name, autor));
-        } if (name != null && !name.isBlank() && id <100 && id != null){
-                    return ResponseEntity.ok(bookService.findBooksByNameOrAutorAndIdGreaterThanIgnoreCase(name, autor, id));
+        }
+        if (name != null && !name.isBlank() && id < 100 && id != null) {
+            return ResponseEntity.ok(bookService.findBooksByNameOrAutorAndIdGreaterThanIgnoreCase(name, autor, id));
         }
         return ResponseEntity.ok(bookService.getAllBooks());
     }
@@ -63,8 +73,18 @@ public class BooksController {
 
     @DeleteMapping("{id}")
     public ResponseEntity deleteBook(@PathVariable Long id) {
-         bookService.deleteBook(id);
+        bookService.deleteBook(id);
         return ResponseEntity.ok().build();
     }
 
+//    метод чтоб загружать файлы
+    @PostMapping(value = "{id}/cover", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<String> upLoadCover(@PathVariable Long id, @RequestParam MultipartFile cover) throws IOException {
+        if (cover.getSize() >= 1024 * 300) {
+            return ResponseEntity.badRequest().body("File is to BIG");
+        }
+
+        bookCoverService.uploadCover(id, cover);
+        return ResponseEntity.ok().build();
+    }
 }
--- src/main/java/pro/ske/demo/services/BookCoverService.java
+++ src/main/java/pro/ske/demo/services/BookCoverService.java
@@ -1,5 +1,74 @@
 package pro.ske.demo.services;
 
-public class BookCoverService {
+import org.apache.tomcat.jni.Buffer;
+import org.hibernate.result.Output;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import pro.ske.demo.model.Book;
+import pro.ske.demo.repositories.BookCoverRepository;
 
+import javax.transaction.Transactional;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static java.nio.file.StandardOpenOption.CREATE_NEW;
+import static org.assertj.core.internal.Paths.getExtension;
+
+@Service
+@Transactional
+public class BookCoverService {
+    @Value("covers")
+    private String coverDir;
+//    название папки в которой будут храниться обложки
+
+    private final BookService bookService;
+    private final BookCoverRepository bookCoverRepository;
+
+    public BookCoverService(BookService bookService, BookCoverRepository bookCoverRepository) {
+        this.bookService = bookService;
+        this.bookCoverRepository = bookCoverRepository;
+    }
+
+    public void uploadCover (Long bookId, MultipartFile file) throws IOException {
+        Book book = bookService.findBook(bookId);
+
+        Path filePath = Path.of(coverDir, bookId + "." + getExtension(file.getOriginalFilename()));
+
+//        метод который проверит папку и если ее нет то он ее создаст
+        Files.createDirectories(filePath.getParent());
+
+//        метод который удаляет файл если он существует (если к примеру более новая версия файла)
+        Files.deleteIfExists(filePath);
+
+//        мы открываем поток данныйх и потом его закрываем
+        try (
+//            ОТКУДА ЧЕРПАТЬ   открываем поток и читаем из нашего файла, который был загружен пользователем
+                InputStream is = file.getInputStream();
+//         КУДА ЧЕРПАТЬ        создаем пустой файл в который будем записывать и передаем пути и в этот пустой файл будем записывать
+             OutputStream os  = Files.newOutputStream(filePath, CREATE_NEW);
+//          СКОЛЬКО ИНФОРМАЦИИ БРАТЬ ЗА РАЗ      и чтоб не черпать чайной ложкой эти файлы - мы создаем буферезированные потоки и сколько байт за раз (1024) мы будем забирать
+             BufferedInputStream bis = new BufferedInputStream(is, 1024);
+//          СКОЛЬКО ИНФОРМАЦИИ ЗАПИСЫВАТЬ ЗА РАЗ      точно такую же операцияю делаем запись по буферу 1024
+             BufferedOutputStream bos = new BufferedOutputStream(os, 1024);
+             ) {
+//            передача информации из вВХОДНОГО потока в ВЫХОДНОЙ поток bis -> bos
+            bis.transferTo(bos);
+        }
+
+
+    }
+
+
+
+
+
+
+
+
+//    метод который находит последнюю точку в нашей строке и возращает все что после точки
+private String getExtension (String fileName) {
+    return fileName.substring(fileName.lastIndexOf(".") + 1);
+}
 }
